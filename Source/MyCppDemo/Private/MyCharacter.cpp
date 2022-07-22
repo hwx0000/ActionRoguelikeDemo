4df240c165cc6bfacde70bf1893649fcaeee0c6c
hwx0000 <wutong@papegames.net>
2022-07-22 14:25:40 +0800

    Use new character model and import its animations

--- Source/MyCppDemo/Private/MyCharacter.cpp
+++ Source/MyCppDemo/Private/MyCharacter.cpp
@@ -25,14 +25,14 @@ void AMyCharacter::BeginPlay()
 
 void AMyCharacter::MovingForwardFunc(float axisValue)
 {
-	UE_LOG(LogTemp, Log, TEXT("Forward = %f"), axisValue);
+	//UE_LOG(LogTemp, Log, TEXT("Forward = %f"), axisValue);
 
 	AddMovementInput(GetActorForwardVector(), axisValue);
 }
 
 void AMyCharacter::MovingRightFunc(float axisValue)
 {
-	UE_LOG(LogTemp, Log, TEXT("Right = %f"), axisValue);
+	//UE_LOG(LogTemp, Log, TEXT("Right = %f"), axisValue);
 	AddMovementInput(GetActorRightVector(), axisValue);
 }
 
